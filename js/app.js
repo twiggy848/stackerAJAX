319c761964fcc8bd4d6a1b88599e900c3724d297
Maile Lucks <maile.lucks@gmail.com>
2016-01-04 20:02:06 -0800

    Updating apps.js with new function names

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
 // this function takes the question object returned by the StackOverflow request
 // and returns new result to be appended to DOM
-var showQuestion = function(question) {
-	
+//var showQuestion = function(question) {
+//
+//	// clone our result template code
+//	var result = $('.templates .question').clone();
+//	
+//	// Set the question properties in result
+//	var questionElem = result.find('.question-text a');
+//	questionElem.attr('href', question.link);
+//	questionElem.text(question.title);
+//
+//	// set the date asked property in result
+//	var asked = result.find('.asked-date');
+//	var date = new Date(1000*question.creation_date);
+//	asked.text(date.toString());
+//
+//	// set the .viewed for question property in result
+//	var viewed = result.find('.viewed');
+//	viewed.text(question.view_count);
+//
+//	// set some properties related to asker
+//	var asker = result.find('.asker');
+//	asker.html('<p>Name: <a target="_blank" '+
+//		'href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
+//		question.owner.display_name +
+//		'</a></p>' +
+//		'<p>Reputation: ' + question.owner.reputation + '</p>'
+//	);
+//console.log("yo");
+//	return result;
+//    
+//};
+
+var showTopQuestions = function(question) {
+
 	// clone our result template code
 	var result = $('.templates .question').clone();
 	
@@ -27,15 +59,15 @@ var showQuestion = function(question) {
 		'</a></p>' +
 		'<p>Reputation: ' + question.owner.reputation + '</p>'
 	);
-
+console.log("yo");
 	return result;
 };
-
 
 // this function takes the results object from StackOverflow
 // and returns the number of results and tags to be appended to DOM
 var showSearchResults = function(query, resultNum) {
 	var results = resultNum + ' results for <strong>' + query + '</strong>';
+    
 	return results;
 };
 
@@ -64,6 +96,7 @@ var getUnanswered = function(tags) {
 		dataType: "jsonp",//use jsonp to avoid cross origin issues
 		type: "GET",
 	})
+    
 	.done(function(result){ //this waits for the ajax to return with a succesful promise object
 		var searchResults = showSearchResults(request.tagged, result.items.length);
 
@@ -71,7 +104,7 @@ var getUnanswered = function(tags) {
 		//$.each is a higher order function. It takes an array and a function as an argument.
 		//The function is executed once for each item in the array.
 		$.each(result.items, function(i, item) {
-			var question = showQuestion(item);
+			var question = showTopQuestions(item);
 			$('.results').append(question);
 		});
 	})
@@ -81,6 +114,47 @@ var getUnanswered = function(tags) {
 	});
 };
 
+var getInspired = function(tags) {
+	
+    var url = "http://api.stackexchange.com/2.2" + tags + "/top-answerers/all_time";
+    var request = {
+        site: 'stackoverflow'
+    };
+    
+	// the parameters we need to pass in our request to StackOverflow's API
+	var result = { 
+		tagged: tags,
+		site: 'stackoverflow',
+		order: 'desc',
+		sort: 'creation'
+	};
+	
+	$.ajax({
+		url: url,
+		data: request,
+		dataType: "jsonp",//use jsonp to avoid cross origin issues
+		type: "GET",
+	})
+    
+	.done(function(result){ //this waits for the ajax to return with a succesful promise object
+		var searchResults = showSearchResults(tag, result.items.length);
+		$('.search-results').html(searchResults);
+		//$.each is a higher order function. It takes an array and a function as an argument.
+		//The function is executed once for each item in the array.
+		$.each(result.items, function(index, item) {
+			var inspiration = showInspiration(item);
+			$('.results').append(inspiration);
+		});
+	})
+	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
+var showInspired = function(inspiration) {
+    
+};
 
 $(document).ready( function() {
 	$('.unanswered-getter').submit( function(e){
