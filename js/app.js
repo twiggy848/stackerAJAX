c9816101453daecd55002ebb0c144394df1105c7
Maile Lucks <maile.lucks@gmail.com>
2016-01-06 10:14:09 -0800

    cleaning up code

--- js/app.js
+++ js/app.js
@@ -148,15 +148,14 @@ var getInspiration = function(tag) {
     var request = {
         site: 'stackoverflow'
     };
-    console.log(url);
-	// the parameters we need to pass in our request to StackOverflow's API
+
 	var result = $.ajax({
 		url: url,
 		data: request,
 		dataType: "jsonp",//use jsonp to avoid cross origin issues
 		type: "GET"  
 	}).done(function(result) { //this waits for the ajax to return with a succesful promise object
-        console.log(result.items);
+        
 		var searchResults = showSearchResults(tag, result.items.length);
 		$('.search-results').html(searchResults);
 		
